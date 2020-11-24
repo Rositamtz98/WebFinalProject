872edced13ddb701c91c34ddf13fe9c358311ebc
SergonJr <sgzzsifuentes@gmail.com>
2020-11-24 16:35:35 -0600

    Deploying

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const uri = "mongodb+srv://Sergon:spiderman99@cluster0.ugamq.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
-mongoose.connect('mongodb://localhost/proyecto-final', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/proyecto-final', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
